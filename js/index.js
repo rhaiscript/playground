5c32b4fa33b477910a305ef171844c7420934c30
Alvin Wong <alvinhochun@gmail.com>
2020-06-26 00:23:02 +0800

    Delay some JS code to run after WASM module is loaded

--- js/index.js
+++ js/index.js
@@ -1,8 +1,5 @@
-import("../pkg/index.js").catch(console.error).then(
-    module => {
-        window.run_script = module.run_script;
-    }
-);
+const wasmImport = import("../pkg/index.js").catch(console.error);
+
 import CodeMirror from "codemirror";
 import "codemirror/lib/codemirror.css";
 import "codemirror/mode/rust/rust.js";
@@ -15,41 +12,43 @@ fn run(a) {
 run(10);
 `;
 
-const editor = CodeMirror(document.getElementById('editorContainer'), {
-    value: initialCode,
-    mode: "rust",
-    lineNumbers: true,
-    indentUnit: 4,
-    extraKeys: {
-        "Tab": cm => {
-            // This function is a modification of `defaultTab` to insert soft
-            // tab instead of hard tab.
-            if (cm.somethingSelected()) {
-                cm.indentSelection("add");
-            } else {
-                cm.execCommand("insertSoftTab");
-            }
+wasmImport.then(module => {
+    const editor = CodeMirror(document.getElementById('editorContainer'), {
+        value: initialCode,
+        mode: "rust",
+        lineNumbers: true,
+        indentUnit: 4,
+        extraKeys: {
+            "Tab": cm => {
+                // This function is a modification of `defaultTab` to insert soft
+                // tab instead of hard tab.
+                if (cm.somethingSelected()) {
+                    cm.indentSelection("add");
+                } else {
+                    cm.execCommand("insertSoftTab");
+                }
+            },
+            "Ctrl-Enter": cm => {
+                doRunScript();
+            },
         },
-        "Ctrl-Enter": cm => {
-            doRunScript();
-        },
-    },
-});
+    });
 
-function doRunScript() {
-    let resultEl = document.getElementById('result');
-    resultEl.value = `Running script at ${new Date().toISOString()}\n\n`;
-    try {
-        let script = editor.getValue();
-        let result = window.run_script(script, s => {
-            resultEl.value += `[PRINT] ${s}\n`;
-        }, s => {
-            resultEl.value += `[DEBUG] ${s}\n`;
-        });
-        resultEl.value += `\nScript returned: "${result}"`;
-    } catch (ex) {
-        resultEl.value += `\nEXCEPTION: "${ex}"`;
+    function doRunScript() {
+        let resultEl = document.getElementById('result');
+        resultEl.value = `Running script at ${new Date().toISOString()}\n\n`;
+        try {
+            let script = editor.getValue();
+            let result = module.run_script(script, s => {
+                resultEl.value += `[PRINT] ${s}\n`;
+            }, s => {
+                resultEl.value += `[DEBUG] ${s}\n`;
+            });
+            resultEl.value += `\nScript returned: "${result}"`;
+        } catch (ex) {
+            resultEl.value += `\nEXCEPTION: "${ex}"`;
+        }
+        resultEl.value += `\nFinished at ${new Date().toISOString()}`;
     }
-    resultEl.value += `\nFinished at ${new Date().toISOString()}`;
-}
-window.btnClick = doRunScript;
+    window.btnClick = doRunScript;
+});
