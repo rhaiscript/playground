9082f8a05cb47d67d7db21e00ccb0ba93ca01b6c
Alvin Wong <alvinhochun@gmail.com>
2020-06-27 19:01:21 +0800

    Add loading screen

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
 import("./playground").catch(console.error).then(module => {
+    document.getElementById("loading").remove();
+    document.getElementById("topContainer").classList.remove("hidden");
     module.initEditor();
 })
